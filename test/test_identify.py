e879ec55ae24fc4247a548d94294360c81f4fddc
Trevor Stanhope <trevor@row-assist>
2015-07-16 15:41:42 -0400

    functioning test_identify

--- test/test_identify.py
+++ test/test_identify.py
@@ -1,6 +1,6 @@
 import cv2, cv
 import numpy as np
-
+import sys
 
 FRAME_WIDTH = 320
 FRAME_HEIGHT = 240
@@ -8,7 +8,7 @@ SATURATION = 1.0
 BRIGHTNESS = 0.5
 CONTRAST = 0.5
 NUM_FLUSH = 30
-FILENAME = 'testmask.jpg'
+FILENAME = sys.argv[1]
 
 #cam = cv2.VideoCapture(0)
 #cam.set(cv.CV_CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
@@ -26,91 +26,87 @@ FILENAME = 'testmask.jpg'
 #    cv2.imwrite(FILENAME, bgr)
 
 # Load image
-bgr=cv2.imread('test5.jpg')
+bgr=cv2.imread(FILENAME)
 bgr=cv2.medianBlur(bgr,5)
+detected_areas = [(0,0,0,0,0)] * 3
 
-## green
-greenlow=np.array([45,0,0])
-greenhigh=np.array([75,255,255])
-
+## Green
+greenlow = np.array([45,0,0])
+greenhigh = np.array([75,255,255])
 hsv = cv2.cvtColor(bgr,cv2.COLOR_BGR2HSV)
 mask = cv2.inRange(hsv,greenlow,greenhigh)
-kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-kernel2=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))
-
-
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))
 opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-
 ret,thresh = cv2.threshold(closing,127,255,0)
 contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-# Find the index of the largest contour
 areas = [cv2.contourArea(c) for c in contours]
-max_index = np.argmax(areas)
-cnt=contours[max_index]
-
+max_index = np.argmax(areas) # Find the index of the largest contour
+cnt = contours[max_index]
 x,y,w,h = cv2.boundingRect(cnt)
-cv2.rectangle(bgr,(x,y),(x+w,y+h),(0,255,0),2)
-cv2.imshow("Show",bgr)
-cv2.waitKey()
-cv2.destroyAllWindows()
+detected_areas[0] = (x, y, w, h)
 
-##yellow
-yellowlow=np.array([15,0,0])
-yellowhigh=np.array([45,255,255])
-
+## Yellow
+yellowlow = np.array([15,0,0])
+yellowhigh = np.array([45,255,255])
 hsv = cv2.cvtColor(bgr,cv2.COLOR_BGR2HSV)
 mask = cv2.inRange(hsv,yellowlow,yellowhigh)
 kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 kernel2=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))
-
-
 opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-
 ret,thresh = cv2.threshold(closing,127,255,0)
 contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-# Find the index of the largest contour
-areas = [cv2.contourArea(c) for c in contours]
-max_index = np.argmax(areas)
-cnt=contours[max_index]
-
+areas = [cv2.contourArea(c) for c in contours] 
+max_index = np.argmax(areas) # Find the index of the largest contour
+cnt = contours[max_index]
 x,y,w,h = cv2.boundingRect(cnt)
-cv2.rectangle(bgr,(x,y),(x+w,y+h),(0,255,255),2)
-cv2.imshow("Show",bgr)
-cv2.waitKey()
-cv2.destroyAllWindows()
+detected_areas[1] = (x, y, w, h)
 
-## brown
-brownlow=np.array([0,0,0])
-brownhigh=np.array([20,255,102])
-
-brownlow1=np.array([150,0,0])
-brownhigh1=np.array([180,255,102])
-
+## Brown
+brownlow = np.array([0,0,0])
+brownhigh = np.array([20,255,102])
+brownlow1 = np.array([150,0,0])
+brownhigh1 = np.array([180,255,102])
 hsv = cv2.cvtColor(bgr,cv2.COLOR_BGR2HSV)
 mask1 = cv2.inRange(hsv,brownlow,brownhigh)
 mask2 = cv2.inRange(hsv,brownlow1,brownhigh1)
-mask3= mask1+mask2
+mask3 = mask1 + mask2
 kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 kernel2=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))
-
-
 opening = cv2.morphologyEx(mask3, cv2.MORPH_OPEN, kernel)
 closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-
 ret,thresh = cv2.threshold(closing,127,255,0)
 contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-# Find the index of the largest contour
-areas = [cv2.contourArea(c) for c in contours]
+areas = [cv2.contourArea(c) for c in contours] # Find the index of the largest contour
 max_index = np.argmax(areas)
-cnt=contours[max_index]
-
+cnt = contours[max_index]
 x,y,w,h = cv2.boundingRect(cnt)
-cv2.rectangle(bgr,(x,y),(x+w,y+h),(0,87,115),2)
+detected_areas[2] = (x, y, w, h)
+
+## Check Largest
+areas = [w*h for (x, y, w, h) in detected_areas]
+max_area = max(areas)
+i = np.argmax(areas)
+(x, y, w, h) = detected_areas[i]
+if i == 0:
+    c = (0,255,0)
+    color = 'green'
+elif i == 1:
+    c = (0,255,255)
+    color = 'yellow'
+elif i == 2:
+    c = (0,87,115)
+    color = 'brown'
+else:
+    exit(1)
+if h > 250:
+    height = 'tall'
+else:
+    height = 'short'
+print height, color
+cv2.rectangle(bgr,(x,y),(x+w,y+h), c, 2) # Draw the rectangle
 cv2.imshow("Show",bgr)
 cv2.waitKey()
 cv2.destroyAllWindows()
