0dc9abe848c0ea94cef91112b23f392063ba5700
Trevor Stanhope <tpstanhope@gmail.com>
2015-07-23 03:47:57 +0000

    better test_command()

--- test/test_commands.py
+++ test/test_commands.py
@@ -1,10 +1,15 @@
-import serial
-s = serial.Serial('/dev/ttyACM0', 9600)
+import serial, sys
+dev = sys.argv[1]
+s = serial.Serial(dev, 9600)
 while True:
     try:
         c = raw_input('Enter command: ')
         s.write(c)
-        r = s.readline()
-        print r
+	while True:
+	    try:
+	    	r = s.readline()
+		print r
+	    except:
+		break
     except KeyboardInterrupt:
         break
